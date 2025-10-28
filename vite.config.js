47dfff26c344eda9210b591301cd12ba4a0b004f
firefly0916 <hermitally@gmail.com>
2025-10-28 14:17:55 +0800

    set default address and port

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -21,5 +20,12 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+
+  // ✅ 新增 server 部分
+  server: {
+    host: '0.0.0.0',  // 允许外部访问（局域网）
+    port: 5173,       // 自定义端口（默认5173）
+    open: true,       // 自动打开浏览器（可选）
   },
 })
